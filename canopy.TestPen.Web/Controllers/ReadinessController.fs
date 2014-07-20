93820c43419d8c1514d8794c31de0ef9bbec7b7e
Chris Holt <lefthandedgoat@gmail.com>
2014-07-20 11:10:27 -0500

    Readiness - Added better colors for Cases per by person pie, and remove pass fail skip none if it was 0

--- canopy.TestPen.Web/Controllers/ReadinessController.fs
+++ canopy.TestPen.Web/Controllers/ReadinessController.fs
@@ -24,51 +24,46 @@ type ReadinessController() =
             |> List.map (fun pfsn -> pfsn.cnt.Value)
             |> List.sum
         
-        let total2 criticality testStatus =
-            pfsns 
-            |> List.filter (fun pfsn -> pfsn.Criticality = criticality && pfsn.TestStatus = testStatus)
-            |> List.map (fun pfsn -> pfsn.cnt.Value)
-            |> List.sum
-        
+        //todo - refator this, has to be a more concise way
         let totals = (total "Pass") + (total "Fail") + (total "Skip") + (total "None")
-        this.ViewData?TotalPass <- (get "High" "Pass") + (get "Medium" "Pass") + (get "Low" "Pass")
-        this.ViewData?TotalFail <- (get "High" "Fail") + (get "Medium" "Fail") + (get "Low" "Fail")
-        this.ViewData?TotalSkip <- (get "High" "Skip") + (get "Medium" "Skip") + (get "Low" "Skip")
-        this.ViewData?TotalNone <- (get "High" "None") + (get "Medium" "None") + (get "Low" "None")
+        this.ViewData?TotalPass <- (total "Pass")
+        this.ViewData?TotalFail <- (total "Fail")
+        this.ViewData?TotalSkip <- (total "Skip")
+        this.ViewData?TotalNone <- (total "None")
         this.ViewData?TotalPassPercent <- percent (total "Pass") totals
         this.ViewData?TotalFailPercent <- percent (total "Fail") totals
         this.ViewData?TotalSkipPercent <- percent (total "Skip") totals
         this.ViewData?TotalNonePercent <- percent (total "None") totals
 
-        let totals = (total2 "High" "Pass") + (total2 "High" "Fail") + (total2 "High" "Skip") + (total2 "High" "None")
+        let totals = (get "High" "Pass") + (get "High" "Fail") + (get "High" "Skip") + (get "High" "None")
         this.ViewData?HighPass <- get "High" "Pass"
         this.ViewData?HighFail <- get "High" "Fail"
         this.ViewData?HighSkip <- get "High" "Skip"
         this.ViewData?HighNone <- get "High" "None"
-        this.ViewData?HighPassPercent <- percent (total2 "High" "Pass") totals
-        this.ViewData?HighFailPercent <- percent (total2 "High" "Fail") totals
-        this.ViewData?HighSkipPercent <- percent (total2 "High" "Skip") totals
-        this.ViewData?HighNonePercent <- percent (total2 "High" "None") totals
+        this.ViewData?HighPassPercent <- percent (get "High" "Pass") totals
+        this.ViewData?HighFailPercent <- percent (get "High" "Fail") totals
+        this.ViewData?HighSkipPercent <- percent (get "High" "Skip") totals
+        this.ViewData?HighNonePercent <- percent (get "High" "None") totals
 
-        let totals = (total2 "Medium" "Pass") + (total2 "Medium" "Fail") + (total2 "Medium" "Skip") + (total2 "Medium" "None")
+        let totals = (get "Medium" "Pass") + (get "Medium" "Fail") + (get "Medium" "Skip") + (get "Medium" "None")
         this.ViewData?MediumPass <- get "Medium" "Pass"
         this.ViewData?MediumFail <- get "Medium" "Fail"
         this.ViewData?MediumSkip <- get "Medium" "Skip"
         this.ViewData?MediumNone <- get "Medium" "None"
-        this.ViewData?MediumPassPercent <- percent (total2 "Medium" "Pass") totals
-        this.ViewData?MediumFailPercent <- percent (total2 "Medium" "Fail") totals
-        this.ViewData?MediumSkipPercent <- percent (total2 "Medium" "Skip") totals
-        this.ViewData?MediumNonePercent <- percent (total2 "Medium" "None") totals
+        this.ViewData?MediumPassPercent <- percent (get "Medium" "Pass") totals
+        this.ViewData?MediumFailPercent <- percent (get "Medium" "Fail") totals
+        this.ViewData?MediumSkipPercent <- percent (get "Medium" "Skip") totals
+        this.ViewData?MediumNonePercent <- percent (get "Medium" "None") totals
         
-        let totals = (total2 "Low" "Pass") + (total2 "Low" "Fail") + (total2 "Low" "Skip") + (total2 "Low" "None")
+        let totals = (get "Low" "Pass") + (get "Low" "Fail") + (get "Low" "Skip") + (get "Low" "None")
         this.ViewData?LowPass <- get "Low" "Pass"
         this.ViewData?LowFail <- get "Low" "Fail"
         this.ViewData?LowSkip <- get "Low" "Skip"
         this.ViewData?LowNone <- get "Low" "None"
-        this.ViewData?LowPassPercent <- percent (total2 "Low" "Pass") totals
-        this.ViewData?LowFailPercent <- percent (total2 "Low" "Pass") totals
-        this.ViewData?LowSkipPercent <- percent (total2 "Low" "Pass") totals
-        this.ViewData?LowNonePercent <- percent (total2 "Low" "Pass") totals
+        this.ViewData?LowPassPercent <- percent (get "Low" "Pass") totals
+        this.ViewData?LowFailPercent <- percent (get "Low" "Fail") totals
+        this.ViewData?LowSkipPercent <- percent (get "Low" "Skip") totals
+        this.ViewData?LowNonePercent <- percent (get "Low" "Pass") totals
         
         this.ViewData?ReadinessRanData <- getReadinessRan id |> JsonConvert.SerializeObject 
         this.ViewData?ReadinessRanByUserByDay <- getRanByUserByDay id |> JsonConvert.SerializeObject 
--- canopy.TestPen.Web/helpers/helper.fs
+++ canopy.TestPen.Web/helpers/helper.fs
@@ -7,8 +7,11 @@ open Newtonsoft.Json
 let (?<-) (viewData:ViewDataDictionary) (name:string) (value:'T) = viewData.Add(name, box value)
 
 let percent numerator denominator = 
-    let result = (decimal numerator) / (decimal denominator) * 100M
-    System.Math.Round(result, 1)
+    if numerator = 0 then 0M
+    else if denominator = 0 then 0M
+    else
+        let result = (decimal numerator) / (decimal denominator) * 100M
+        System.Math.Round(result, 1)
 
 let toJson sectionId (reports : data.getReportsQuery.Record list) =     
     let reports =
